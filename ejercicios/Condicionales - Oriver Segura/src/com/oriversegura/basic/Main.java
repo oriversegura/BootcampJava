2ed1dad54929154afc9b8fdae16685629df1eda0
oriversegura <oriverseguravargas@gmail.com>
2025-10-30 14:36:24 -0400

    add E.6.4 firt part

--- ejercicios/Condicionales - Oriver Segura/src/com/oriversegura/basic/Main.java
+++ ejercicios/Condicionales - Oriver Segura/src/com/oriversegura/basic/Main.java
@@ -1,8 +1,12 @@
 package com.oriversegura.basic;
 
+import com.oriversegura.basic.utils.StringUtils;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		System.out.println("0 to Hero Java 2025");
 
 	}
 
--- ejercicios/Condicionales - Oriver Segura/src/com/oriversegura/basic/utils/StringUtils.java
+++ ejercicios/Condicionales - Oriver Segura/src/com/oriversegura/basic/utils/StringUtils.java
@@ -2,14 +2,35 @@ package com.oriversegura.basic.utils;
 
 public class StringUtils {
 
+//	E6.4
+//	CamelCase es una forma de escribir identificadores con más de una palabra “separándolos”
+//	usando letras mayúsculas. El caso serpiente, por otro lado, usa letras minúsculas pero guiones
+//	bajos para separar palabras. Escriba una función camelCaseToSnakeCase que tome una String
+//	que sea un identificador válido escrito en CamelCase y la devuelva en Snake Case. Además,
+//	escribe una función snakeCaseToCamelCase para la operación inversa.
+
 	public static String snakeCaseToCamelCase(String s) {
 		var builder = new StringBuilder();
 
 		return builder.toString();
+
 	}
 
 	public static String camelCaseToSnakeCase(String s) {
 		var builder = new StringBuilder();
+
+		for (int letra = 0; letra < s.length(); letra++) {
+
+			if (Character.isUpperCase(s.charAt(letra)) == true) {
+				builder.append('_');
+
+				builder.append((s.toLowerCase().charAt(letra)));
+
+			} else {
+				builder.append(s.charAt(letra));
+			}
+
+		}
 
 		return builder.toString();
 	}
